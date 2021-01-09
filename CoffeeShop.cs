2a256704ec88907aa2c029784ecff9f7a4b1b87f
syedakinza <kinza.fatima2511@yahoo.com>
2021-01-09 14:33:15 +0500

    Version Control Lab

---
+++ CoffeeShop.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShopApplication
+{
+    class CoffeeShop
+    {
+        double noOfCups, cusCups;
+        string name, contactno;
+        double billAmount;
+        public CoffeeShop(double noOfCups)
+        {
+            this.noOfCups = noOfCups;
+        }
+        public bool addCups(double Cups)
+        {
+            if (Cups > 0)
+            {
+                this.noOfCups += Cups;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public void supplyCups(int Cups)
+        {
+            this.noOfCups -= Cups;
+        }
+        public bool newCustomer(string name,string contactno,int cups)
+        {
+            if (cups <= this.noOfCups)
+            {
+                this.name = name;
+                this.contactno = contactno;
+                this.cusCups = cups;
+                supplyCups(cups);
+                this.billAmount = CalculateBill(cups);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("We are sorry! We don't have " + cups + " cups of coffee.");
+                return false;
+            }
+        }
+        public double CalculateBill(int cups)
+        {
+            return cups * 120;
+        }
+        public double TotalCups()
+        {
+            return this.noOfCups;
+        }
+
+        public void DisplayCustomerData()
+        {
+            Console.WriteLine("Name : "+this.name);
+            Console.WriteLine("Contact Number : "+this.contactno);
+            Console.WriteLine("No of Coffee Cups : "+this.cusCups);
+            Console.WriteLine("Total bill : " + this.billAmount);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShopApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CoffeeShop newShop = new CoffeeShop(30);
+            Console.WriteLine("-----WELLCOME TO THE NEW COFFEE SHOP-----");
+            Console.WriteLine("Initially we have "+newShop.TotalCups()+" cups"); 
+            while (true)
+            {
+                Console.WriteLine("1-Customer\n2-Owner");
+                Console.Write("Enter here : ");
+                int choice = int.Parse(Console.ReadLine());
+                if (choice == 1)
+                {
+                    Console.Write("Enter your name : ");
+                    string name = Console.ReadLine();
+                    Console.Write("Enter your contact number : ");
+                    string cno = Console.ReadLine();
+                    Console.Write("Enter number of cups you want : ");
+                    int cups = int.Parse(Console.ReadLine());
+                    if(newShop.newCustomer(name, cno, cups))
+                    {
+                        newShop.DisplayCustomerData();
+                    }    
+                }
+                else if (choice == 2)
+                {
+                    Console.WriteLine("1-Want to add more coffee cups \n2-Show Total cups");
+                    Console.Write("Enter here : ");
+                    int c = int.Parse(Console.ReadLine());
+                    if (c == 1)
+                    {
+                        Console.WriteLine("How many cups you want to add?");
+                        Console.WriteLine("Enter amount : ");
+                        int newCups = int.Parse(Console.ReadLine());
+                        if (newShop.addCups(newCups))
+                        {
+                            Console.WriteLine("Cups are added successfully");
+                        } 
+                        else
+                        {
+                            Console.WriteLine("Sorry! Can't add cups. Enter valid amount of cups");
+                        }
+                    }
+                    else if (c == 2)
+                    {
+                        Console.WriteLine("Total Coffee Cups are "+newShop.TotalCups());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Enter Valid Choice");
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+            
+
+        }
+    }
+}
